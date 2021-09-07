e062fb92e65c07699b62f6a164050758ff12e000
invalidtoken <codeit.sahil98@gmail.com>
2021-09-07 17:38:46 +0530

    fixing import issues

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {render} from '../../../test/calculator-test-utils'
-import CalculatorDisplay from '../calculator-display'
+import {render} from 'calculator-test-utils'
+import CalculatorDisplay from 'calculator-display'
 
 test('renders', () => {
   const {container} = render(<CalculatorDisplay value="0" />)
